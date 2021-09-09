8ff0d0d1c819201a35630fd741e5d4a3c531bec1
Esteban <estebanvalladar@gmail.com>
2021-09-09 20:34:20 -0300

    3er commit: El programa esta terminado.

--- potencia.php
+++ potencia.php
@@ -12,6 +12,38 @@ echo "Potencia de Ambos Numeros";
 echo "<br>";
 echo "<br>";
 
+for ($i=1; $i<=$exp; $i++) // Aqui se calculan Ambas portencias
+ { 
+	 $pot=pow($N1,$i);
+	 $potenciaN1[$i]=$pot;
+	
+ }
+
+for ($j=1; $j<=$exp ; $j++)
+ { 
+	 $pot=pow($N2,$j);
+	 $potenciaN2[$j]=$pot;
+	 
+ }
+
+ for ($i=1; $i<=$exp ; $i++) // Aqui se muestran
+ { 
+	 
+	echo "las potencias de {$N1} a la {$i} potencia son ".$potenciaN1[$i];
+	echo "<br>";
+ }
+ echo "<br>";
+
+ for ($j=1; $j<=$exp; $j++) // Aqui se muestran
+ { 
+	 
+	echo "las potencias de {$N2} a la {$j} potencia son ".$potenciaN2[$j];
+	echo "<br>";
+ }
+
+
+
+
 
 
 
