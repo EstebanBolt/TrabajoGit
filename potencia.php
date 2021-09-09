c9963fa93fc07b4d67cdd4fd383877be4a575951
Esteban <estebanvalladar@gmail.com>
2021-09-09 19:48:37 -0300

    Primer commit: El siguiente programa calculara la potencia de numeros elegidos por el usuario.

---
+++ potencia.php
@@ -0,0 +1,23 @@
+<?php
+$N1=$_GET['n1'];
+$N2=$_GET['n2'];
+$exp=$_GET['exp'];
+$pot=0;
+$potenciaN1=array();
+$potenciaN2=array();
+
+echo "Calcule la potencia de 2 numeros X";
+echo "<br>";
+echo "Potencia de Ambos Numeros";
+echo "<br>";
+
+
+
+
+
+
+
+
+
+
+?>
